f8cca7506a9335f307f9323fc7385fca1c351882
chrisoc19 <chrisoconnor904@yahoo.com.au>
2019-11-18 14:06:15 +0000

    added nav bar and home.html

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'workout_app'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-fwzhc.mongodb.net/workout_app?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_exercise')
+def get_exercise():
+    return render_template("exercises.html", exercises=mongo.db.exercise.find())
+
+@app.route('/home_page')
+def go_home():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
